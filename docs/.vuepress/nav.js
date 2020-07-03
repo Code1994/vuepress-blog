9d8ae88f93d27c5edc0bc183bc3fedd9c8f90087
yangxiaoping <2878847069@qq.com>
2020-07-03 10:49:32 +0800

    init

---
+++ docs/.vuepress/nav.js
@@ -0,0 +1,74 @@
+module.exports = [
+  // 内部链接 以docs为根目录
+  { 
+    text: '前端', 
+    items: [
+      {
+        text: 'HTML5',
+        link: '/FE/HTML5/'
+      },
+      {
+        text: 'CSS3',
+        link: '/FE/CSS3/'
+      },
+      {
+        text: 'JavaScript',
+        link: '/FE/JavaScript/'
+      },
+      {
+        text: 'TCP/IP',
+        link: '/FE/TCP/'
+      },
+      {
+        text: '跨平台',
+        link: '/FE/cross'
+      },
+      {
+        text: '工程化',
+        link: '/FE/Engineer/'
+      }
+    ] 
+  }, 
+  { 
+    text: '后端', 
+    link: '/BE/',
+    items: [
+      {
+        text: 'Node',
+        link: '/BE/Node/'
+      }
+    ]
+  },
+  {
+    text: '数据库',
+    link: '/DB/',
+    items: [
+      {
+        text: 'MySQL',
+        link: '/DB/MySQL/'
+      },
+      {
+        text: 'MongoDB',
+        link: '/DB/MongoDB/'
+      }
+    ]
+  },
+  {
+    text: '部署',
+    link: '/deploy/',
+    items: [
+      {
+        text: 'Nginx',
+        link: '/deploy/Nginx/'
+      },
+      {
+        text: 'Docker',
+        link: '/deploy/Docker/'
+      }
+    ]
+  },
+  {
+    text: 'GitHub',
+    link: 'https://github.com/Code1994'
+  }
+]
