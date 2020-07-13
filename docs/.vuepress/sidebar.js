baa34e82d08473cda43ec2175962b3e0f6da70e8
yangxiaoping <2878847069@qq.com>
2020-07-13 15:18:47 +0800

    feat: nginx start

--- docs/.vuepress/sidebar.js
+++ docs/.vuepress/sidebar.js
@@ -1,11 +1,17 @@
 // 数组形式 所有的侧边栏都一样 而且该侧边栏包含所有配置路由
 // 对象形式 侧边栏分为多个 每个侧边栏只会匹配当前满足的路由
 module.exports = {
+  // 前端基础
   '/FE/HTML5/': [
     '/FE/HTML5/',
     '/FE/HTML5/1.semantic.md',
     '/FE/HTML5/5.graphic.md',
   ],
   '/FE/CSS3/': [
+  ],
+  // 部署
+  '/deploy/Nginx/': [
+    '/deploy/Nginx/',
+    '/deploy/Nginx/1.install.md'
   ]
 }
