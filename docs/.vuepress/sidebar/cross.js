d873359c7e86675ca1154d5ee0a9e042bdb3ca78
yangxiaoping <2878847069@qq.com>
2021-04-05 00:46:01 +0800

    feat: 微信JS-SDK自定义文案分享

--- docs/.vuepress/sidebar/cross.js
+++ docs/.vuepress/sidebar/cross.js
@@ -2,6 +2,7 @@
 module.exports = {
   '/cross/h5/': [],
   '/cross/weChat-public/': [
-    '/cross/weChat-public/1.authorization.md'
+    '/cross/weChat-public/1.authorization.md',
+    '/cross/weChat-public/2.share.md'
   ]
 }
